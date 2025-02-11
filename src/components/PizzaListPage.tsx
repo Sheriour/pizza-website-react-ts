1e96969d83074b1aa9ca579b8aa2ea475cf922d0
Marek Dziekan <sheriour@gmail.com>
2025-02-11 23:27:00 +0000

    Generator add to archive from preview and QoL improvements.

--- src/components/PizzaListPage.tsx
+++ src/components/PizzaListPage.tsx
@@ -1,16 +1,32 @@
 import { Pizza } from "../Types";
 import CreatedPizzaListItem from "./CreatedPizzaListItem";
+import SimpleButton from "./SimpleButton";
 
 type PizzaListProps = {
   createdPizzas: Pizza[];
-  onDeletePizza: () => void;
+  onDeletePizza: (pizza: Pizza) => void;
 };
 
 function PizzaList({ createdPizzas, onDeletePizza }: PizzaListProps) {
+  const deleteAllFromArchive = () => {
+    for (const pizza of createdPizzas) onDeletePizza(pizza);
+  };
+
   return (
     <>
       <div className="container border mt-2 pb-3">
         <h4 className={"text-center mt-3"}>Pizza Archive</h4>
+
+        {createdPizzas.length === 0 ? (
+          <p className="text-center">The archive is empty.</p>
+        ) : (
+          <div className="container mt-3">
+            <SimpleButton
+              buttonText="Clear Archive"
+              handleOnClick={deleteAllFromArchive}
+            ></SimpleButton>
+          </div>
+        )}
 
         <div className={"container mt-2"}>
           <div className="list-group" id="pizza-list">
